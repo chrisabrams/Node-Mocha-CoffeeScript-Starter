13d892daa9b3ece66537a80d3a0a60c013e439e3
Sebastian Schürmann <sebs@2xs.org>
2014-07-13 16:33:58 +0200

    help added

--- gulpfile.js
+++ gulpfile.js
@@ -6,43 +6,38 @@ var ext_replace = require("gulp-ext-replace");
 var rm = require( 'gulp-rm' );
 var watch = require('gulp-watch');
 
+var taskListing = require('gulp-task-listing');
+
+// Add a task to render the output
+gulp.task('help', taskListing);
+
+// coffee lint over the source
 gulp.task('lint-src', function () {
     gulp.src('./src/*.coffee')
         .pipe(coffeelint())
         .pipe(coffeelint.reporter());
 });
 
+// coffee lint over the test code
 gulp.task('lint-test', function () {
     gulp.src('./test/unit/*.coffee')
         .pipe(coffeelint())
         .pipe(coffeelint.reporter());
 });
-
-gulp.task('doc', function(){
-  gulp.src("./src/*.coffee")
-    .pipe(markdox())
-    .pipe(gulp.dest("./doc/"));
-
-});
-
+// write the api docs
 gulp.task('doc', function(){
   gulp.src("./src/*.coffee")
     .pipe(markdox())
     .pipe(ext_replace('.md'))
     .pipe(gulp.dest("./doc/"));
 });
-
-gulp.task('rename', function(){
-  gulp.src('./doc/*.coffee')
-      .pipe(ext_replace('.md'))
-      .pipe(gulp.dest('./doc'));
-});
-
+// run tests
 gulp.task('test', function () {
     return gulp.src('test/unit/*.coffee', {read: false})
         .pipe(mocha({reporter: 'nyan', require: 'coffee-script'}));
 });
 
+// clean all build data
 gulp.task('clean', function() {
   return gulp.src( './doc', { read: false })
     .pipe( rm() );
